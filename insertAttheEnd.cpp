af4f02b1a07c534d49ae15ddd7546208592d345c
Nashim Biswakarma <nashimbiswakarma52@gmail.com>
2023-03-27 00:08:08 +0530

    Insert node at the end.

---
+++ insertAttheEnd.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{   
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data=x;
+        next=nullptr;
+    }
+};
+
+// reference to the first pointer (struct node *& first)
+void creatingNodes(Node *&head)
+{
+    Node *temp;
+    int choice=1;
+    while (choice)
+    {
+        int data;
+        cout<<"Enter the data int the list:\n";
+        cin>>data;
+        Node *newnode=new Node(data);
+        if(head==0)
+            head=temp=newnode;
+        else{
+            temp->next=newnode;
+            temp=newnode;
+        }
+        cout<<"Enter 1 to continue:\n";
+        cin>>choice;
+    }
+}
+void display(Node *head)
+{
+    if(head==nullptr)
+        return;
+    else
+    {
+        cout<<head->data<<" ";
+        display(head->next);
+    }
+}
+
+void insertAtTheEnd(Node *&head,int key)
+{
+    if(head==NULL)
+    {
+        Node *newnode=new Node(key);
+        head=newnode;
+    }
+    else
+    {
+        Node* temp=head;
+        while (temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        Node* newnode =new Node(key);
+        temp->next=newnode;
+    }
+
+}
+int main(int argc, char const *argv[])
+{
+    Node *head=0;
+    creatingNodes(head);
+    cout<<"Linked list are as follows:\n";
+    display(head);
+    cout<<"\nEnter the element to insert at the end:\n";
+    int key;
+    cin>>key;
+    insertAtTheEnd(head,key);
+    cout<<"\nUpdated Linked list are as follows:\n";
+    display(head);
+    return 0;
+}
